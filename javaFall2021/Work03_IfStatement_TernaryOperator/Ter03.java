abe7dbb35e7fe89c2ac8c33a1e55296be940938c
Elif Deniz <94469409+ElifSena55@users.noreply.github.com>
2021-12-25 23:06:40 +0300

    java

---
+++ javaFall2021/Work03_IfStatement_TernaryOperator/Ter03.java
@@ -0,0 +1,14 @@
+package _03_IfStatement_TernaryOperator;
+
+import java.util.Scanner;
+
+public class Ter03 {
+
+    public static void main(String[] args) {
+        // Kullanıcıdan bir pozitif tamsayı girmesini isteyin, o pozitif tamsayı 3 basamaklı ise ekrana “3 Basamaklı�? yazdırın.
+        // 3 basamaklı degilse çift olup olmadigini kontrol edin. Çift ise “3 basamaklı olmayan çift sayı�? yazdırın.
+        // Çift sayı degilse “3 basamaklı olmayan tek sayı yazdırın.�?
+
+      
+    }
+}
